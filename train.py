fad14075ecf917295c8ff7812cf57f37ad47956d
gkgo <91817417+gkgo@users.noreply.github.com>
2023-03-06 16:20:44 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,99 +1,98 @@
 import torch
-from torch.utils.data import Dataset, DataLoader
 import torchvision
-import torch.nn as nn
-import torch.nn.functional as F
+import torchvision.transforms as transforms
 import torch.optim as optim
-from torchvision import datasets, transforms
-import time
+import torch.nn as nn
+from torch.utils.data import DataLoader
 from torchvision.datasets import ImageFolder
-from tqdm import tqdm  # 添加进度条库
+import time
+from tqdm import tqdm
+from resnet import resnet18gai
 
-GPU = torch.cuda.is_available()
+# Set device to GPU if available
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+# Define transforms
+train_transform = transforms.Compose([
+    transforms.RandomResizedCrop(224),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+val_transform = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
 
-def train(epoch, model, device, train_loader, optimizer):
-    model.train()
-    size = len(train_loader.dataset)
-    train_loss = 0
+# Load data
+train_data = ImageFolder('./cropped/train', transform=train_transform)
+val_data = ImageFolder('./cropped/test', transform=val_transform)
+
+train_loader = DataLoader(train_data, batch_size=32, shuffle=True, num_workers=2)
+val_loader = DataLoader(val_data, batch_size=32, shuffle=False, num_workers=2)
+
+# Load pre-trained ResNet-18 model
+model = resnet18gai().to(device)
+
+# Define loss function and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr =0.001,momentum=0.9,nesterov=True, weight_decay=0.0005)
+lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[60, 70], gamma=0.05)
+
+# Train the model
+num_epochs = 50
+best_acc = 0.0
+for epoch in range(num_epochs):
+    train_loss = 0.0
     train_correct = 0
-    for batch_idx, (data, target) in enumerate(
-            tqdm(train_loader, desc=f"Training epoch {epoch}", unit="batch", leave=True)):
-        data, target = data.to(device), target.to(device)
-        optimizer.zero_grad()
-        output = model(data)
+    train_total = 0
+    val_loss = 0.0
+    val_correct = 0
+    val_total = 0
+    start_time = time.time()
 
-        loss = F.cross_entropy(output, target)
-        loss.backward()
-        optimizer.step()
-        train_loss += loss.item()
-        train_correct += output.argmax(dim=1).eq(target).sum().item()
+    # Train
+    model.train()
+    with tqdm(train_loader, unit="batch") as tepoch:
+        tepoch.set_description(f"Epoch {epoch+1}/{num_epochs}")
+        for inputs, labels in tepoch:
+            inputs, labels = inputs.to(device), labels.to(device)
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            train_loss += loss.item() * inputs.size(0)
+            _, predicted = torch.max(outputs.data, 1)
+            train_total += labels.size(0)
+            train_correct += (predicted == labels).sum().item()
+            tepoch.set_postfix(loss=train_loss/train_total, acc=train_correct/train_total)
 
-    train_loss /= len(train_loader)
-    train_acc = 100. * train_correct / size
-    print(f"Train Epoch: {epoch}\tLoss: {train_loss:.4f}\tAccuracy: {train_acc:.2f}%")
-
-
-
-def test(model, device, test_loader):
+    # Validate
     model.eval()
-    test_loss = 0
-    test_correct = 0
     with torch.no_grad():
-        for batch_idx, (data, target) in enumerate(tqdm(test_loader, desc="Testing", unit="batch", leave=True)):
-            data, target = data.to(device), target.to(device)
-            output = model(data)
-            loss = F.cross_entropy(output, target)
-            test_loss += loss.item()
-            test_correct += output.argmax(dim=1).eq(target).sum().item()
-        test_loss /= len(test_loader)
-        test_acc = 100. * test_correct / len(test_loader.dataset)
-    print(f"Test set: Average loss: {test_loss:.4f}\tAccuracy: {test_acc:.2f}%")
-    return test_acc
+        with tqdm(val_loader, unit="batch") as tepoch:
+            tepoch.set_description(f"Epoch {epoch+1}/{num_epochs}")
+            for inputs, labels in tepoch:
+                inputs, labels = inputs.to(device), labels.to(device)
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+                val_loss += loss.item() * inputs.size(0)
+                _, predicted = torch.max(outputs.data, 1)
+                val_total += labels.size(0)
+                val_correct += (predicted == labels).sum().item()
+                tepoch.set_postfix(loss=val_loss/val_total, acc=val_correct/val_total)
 
-def main():
-    device = torch.device("cuda" if GPU else "cpu")
+    lr_scheduler.step()
+    end_time = time.time()
+    print(f"Epoch {epoch+1}/{num_epochs} took {end_time - start_time:.2f} seconds")
+    print(f"Train loss: {train_loss/train_total:.4f}, Train accuracy: {train_correct/train_total:.4f}")
+    print(f"Validation loss: {val_loss/val_total:.4f}, Validation accuracy: {val_correct/val_total:.4f}")
+    # Update best accuracy
+    if val_correct / val_total > best_acc:
+        best_acc = val_correct / val_total
 
-    train_transform = transforms.Compose([
-        transforms.RandomResizedCrop(224),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ])
-    val_transform = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ])
-
-    # Load data
-    train_data = ImageFolder('./cropped/train', transform=train_transform)
-    test_data = ImageFolder('./cropped/test', transform=val_transform)
-
-    train_loader = DataLoader(train_data, batch_size=32, shuffle=True, num_workers=0)
-    test_loader = DataLoader(test_data, batch_size=32, shuffle=False, num_workers=0)
-
-    model = torchvision.models.resnet50(pretrained=False)
-    num_features = model.fc.in_features
-    model.fc = nn.Linear(num_features, len(train_data.classes))
-    model = model.to(device)
-    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9, nesterov=True, weight_decay=0.0005)
-    lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[30, 40], gamma=0.05)
-    best_acc = 0.0
-
-    for epoch in range(50):
-        start_time = time.time()
-        train(epoch, model, device, train_loader, optimizer)
-        test_acc = test(model, device, test_loader)
-        lr_scheduler.step()
-        epoch_time = time.time() - start_time
-        if test_acc > best_acc:
-            best_acc = test_acc
-        print(f"Best Accuracy: {best_acc:.2f}%")
-        print(f'[ log ] roughly {(50 - epoch) / 3600. * epoch_time:.2f} h left\n')
-
-
-if __name__ == '__main__':
-    main()
+print(f"Best validation accuracy: {best_acc:.4f}")
 
